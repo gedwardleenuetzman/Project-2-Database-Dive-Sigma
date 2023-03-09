bcf5f97412d5605ea5e77da33278e7f7e340619d
Ralph Vicente <ralphvicente@tamu.edu>
2023-03-09 13:56:29 -0600

    SQL files push

--- datagen.py
+++ datagen.py
@@ -1,35 +1,39 @@
-import csv
 import random
 
-# Define the items for sale
-items = ["Chicken_Sandwich", "Spicy_Sandwich", "Deluxe_Sandwich", "Spicy_Deluxe", "Chicken_Biscuit",
-         "Spicy_Biscuit", "Hash_Browns", "Four_Nuggets", "Eight_Nuggets", "Twelve_Nuggets", "Cobb_Salad",
-         "Market_Salad", "Waffle_Fries", "Side_Salad", "Waffle_Chips", "Frosted_Lemonade", "Frosted_Coffee",
-         "Chocolate_Cookie", "Lemonade", "Ice_Tea", "Sweet_Tea", "Water_Bottle", "CFA_Sauce", "Ranch_Sauce",
-         "Honey_Mustard"]
+# Generate random SQL insert statements with sequential order IDs and write to file
+num_inserts = 50
+order_id = 1
 
-# Generate sales data for each day
-num_days = 365
-sales_data = {}
-for item in items:
-    sales_data[item] = [random.randint(100, 500) for _ in range(num_days)]
+with open("inserts.sql", "w") as f:
+    for i in range(num_inserts):
+        num_products = random.randint(1, 5)
+        products = random.sample(range(0, 26), num_products)
+        quantities = [random.randint(1, 10) for _ in range(num_products)]
 
-# Generate two special days with larger sales numbers
-special_days = []
-while len(special_days) < 2:
-    special_day = random.randint(14, num_days - 14)  # Pick a random day at least 2 weeks apart from the previous special day
-    if all(abs(special_day - prev_special_day) >= 14 for prev_special_day in special_days):
-        for item in items:
-            sales_data[item][special_day] = random.randint(1000, 2000)
-        special_days.append(special_day)
+        sql_values = []
+        for j in range(num_products):
+            product_id = products[j]
+            quantity = quantities[j]
+            sql_values.append(f"({order_id}, {product_id}, {quantity})")
 
-# Write the sales data to a .csv file
-with open("sales_data.csv", "w", newline="") as csvfile:
-    writer = csv.writer(csvfile)
-    header = ["Day"] + items
-    writer.writerow(header)
-    for day in range(1, num_days + 1):
-        row = [day]
-        for item in items:
-            row.append(sales_data[item][day-1])
-        writer.writerow(row)
+        sql_insert = f"INSERT INTO order_products (order_id, prod_id, quantity) VALUES {','.join(sql_values)};"
+        f.write(sql_insert + "\n")
+        
+        order_id += 1
+
+num_inserts = 1000
+
+days_per_order = 1
+
+if num_inserts > 365:
+    days_per_order = num_inserts // 365 + 1
+
+with open("inserts2.sql", "w") as f:
+    sql_inserts = []
+    for i in range(num_inserts):
+        id = i + 1
+        totalprice = round(random.uniform(1.55, 40), 2)
+        day = i // days_per_order + 1
+        sql_insert = f"({id}, {totalprice:.2f}, {day})"
+        sql_inserts.append(sql_insert)
+    f.write("INSERT INTO orders (id, totalprice, day) VALUES " + ", ".join(sql_inserts) + ";")
